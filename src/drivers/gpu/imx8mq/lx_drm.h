abf9f24b8f3910948efd2931d84ee8503c15b334
Josef Söntgen <josef.soentgen@genode-labs.com>
2021-12-09 16:50:12 +0100

    imx8mq_gpu_drv: only invalidate read buffers -- WIP

--- src/drivers/gpu/imx8mq/lx_drm.h
+++ src/drivers/gpu/imx8mq/lx_drm.h
@@ -24,13 +24,20 @@ void  lx_drm_close(void *);
 int       lx_drm_ioctl_etnaviv_gem_param(void *, unsigned char, unsigned long long*);
 int       lx_drm_ioctl_etnaviv_gem_submit(void *, unsigned long, unsigned int*);
 unsigned  lx_drm_gem_submit_bo_count(void const*);
-unsigned *lx_drm_gem_submit_bo_handle(void*, unsigned);
-int       lx_drm_ioctl_etnaviv_gem_new(void *, unsigned long, unsigned int *);
-int       lx_drm_ioctl_etnaviv_gem_info(void *, unsigned int, unsigned long long *);
-int       lx_drm_ioctl_etnaviv_cpu_prep(void *, unsigned int, int);
-int       lx_drm_ioctl_etnaviv_cpu_fini(void *, unsigned int);
-int       lx_drm_ioctl_gem_close(void *, unsigned int);
-int       lx_drm_ioctl_etnaviv_wait_fence(void *, unsigned int);
+
+struct lx_bo
+{
+	unsigned *handle;
+	unsigned  flags;
+};
+
+struct lx_bo lx_drm_gem_submit_bo_handle(void*, unsigned);
+int          lx_drm_ioctl_etnaviv_gem_new(void *, unsigned long, unsigned int *);
+int          lx_drm_ioctl_etnaviv_gem_info(void *, unsigned int, unsigned long long *);
+int          lx_drm_ioctl_etnaviv_cpu_prep(void *, unsigned int, int);
+int          lx_drm_ioctl_etnaviv_cpu_fini(void *, unsigned int);
+int          lx_drm_ioctl_gem_close(void *, unsigned int);
+int          lx_drm_ioctl_etnaviv_wait_fence(void *, unsigned int);
 
 
 #ifdef __cplusplus
