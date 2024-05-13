8d83f508e7de4bf1ba2218070c380db2d12344e5
Josef Söntgen <josef.soentgen@genode-labs.com>
2024-05-13 18:03:34 +0200

    usb_host/imx8mq: use linux-imx 6.1.20
    This commits introduces an updated driver that uses the current
    linux-imx (6.1.20) version. It was tested on the iMX8Q-EVK board
    and MNT Reform 2 with a HID device.

---
+++ src/drivers/usb_host/imx8mq/dummies.c
@@ -0,0 +1,906 @@
+/*
+ * \brief  Dummy definitions of Linux Kernel functions - handled manually
+ * \author Stefan Kalkowski
+ * \date   2021-08-31
+ */
+
+/*
+ * Copyright (C) 2021 Genode Labs GmbH
+ *
+ * This file is distributed under the terms of the GNU General Public License
+ * version 2.
+ */
+
+#include <lx_emul.h>
+
+
+#include <linux/threads.h>
+unsigned long __per_cpu_offset[NR_CPUS] = { 0UL };
+
+
+#include <linux/fs.h>
+
+int __register_chrdev(unsigned int major,unsigned int baseminor,unsigned int count,const char * name,const struct file_operations * fops)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/pci.h>
+
+int __pci_register_driver(struct pci_driver * drv,struct module * owner,const char * mod_name)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/pm_runtime.h>
+
+void __pm_runtime_disable(struct device * dev,bool check_resume)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/pm_runtime.h>
+
+
+void __pm_runtime_use_autosuspend(struct device * dev,bool use)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/pm_runtime.h>
+
+int __pm_runtime_set_status(struct device * dev,unsigned int status)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/pm_runtime.h>
+
+int __pm_runtime_suspend(struct device * dev,int rpmflags)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/dma-map-ops.h>
+
+void arch_setup_dma_ops(struct device * dev,u64 dma_base,u64 size,const struct iommu_ops * iommu,bool coherent)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/cdev.h>
+
+int cdev_add(struct cdev * p,dev_t dev,unsigned count)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/clk.h>
+
+void clk_bulk_disable(int num_clks,const struct clk_bulk_data * clks)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/clk.h>
+
+int __must_check clk_bulk_get_all(struct device * dev,struct clk_bulk_data ** clks)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/clk.h>
+
+int __must_check clk_bulk_enable(int num_clks,const struct clk_bulk_data * clks)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/clk.h>
+
+int __must_check clk_bulk_prepare(int num_clks,const struct clk_bulk_data * clks)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/clk.h>
+
+void clk_bulk_unprepare(int num_clks,const struct clk_bulk_data * clks)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/pm_qos.h>
+
+int dev_pm_qos_expose_flags(struct device * dev,s32 val)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/pm_qos.h>
+
+int dev_pm_qos_add_request(struct device * dev,struct dev_pm_qos_request * req,enum dev_pm_qos_req_type type,s32 value)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/pm_qos.h>
+
+int dev_pm_qos_remove_request(struct dev_pm_qos_request * req)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/regulator/consumer.h>
+
+struct regulator * devm_regulator_get(struct device * dev,const char * id)
+{
+	lx_emul_trace(__func__);
+	return NULL;
+}
+
+
+#include <linux/reset.h>
+
+struct reset_control * devm_reset_control_array_get(struct device * dev,bool shared,bool optional)
+{
+	lx_emul_trace(__func__);
+	return NULL;
+}
+
+
+extern int devtmpfs_create_node(struct device * dev);
+int devtmpfs_create_node(struct device * dev)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+extern int devtmpfs_delete_node(struct device * dev);
+int devtmpfs_delete_node(struct device * dev)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+extern void dpm_sysfs_remove(struct device * dev);
+void dpm_sysfs_remove(struct device * dev)
+{
+	lx_emul_trace(__func__);
+}
+
+
+struct dwc3;
+
+extern void dwc3_debugfs_exit(struct dwc3 * dwc);
+void dwc3_debugfs_exit(struct dwc3 * dwc)
+{
+	lx_emul_trace(__func__);
+}
+
+
+extern void dwc3_debugfs_init(struct dwc3 * dwc);
+void dwc3_debugfs_init(struct dwc3 * dwc)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/mm.h>
+
+DEFINE_STATIC_KEY_FALSE(init_on_alloc);
+DEFINE_STATIC_KEY_FALSE(init_on_free);
+
+
+#include <linux/ioport.h>
+
+int insert_resource(struct resource * parent,struct resource * new)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+extern void irq_pm_install_action(struct irq_desc * desc,struct irqaction * action);
+void irq_pm_install_action(struct irq_desc * desc,struct irqaction * action)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/mm.h>
+
+bool is_vmalloc_addr(const void * x)
+{
+	lx_emul_trace(__func__);
+	return false;
+}
+
+
+#include <linux/kernfs.h>
+
+void kernfs_put(struct kernfs_node * kn)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/clk/clk-conf.h>
+
+int of_clk_set_defaults(struct device_node * node,bool clk_supplier)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/pinctrl/devinfo.h>
+
+int pinctrl_bind_pins(struct device * dev)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/pinctrl/devinfo.h>
+
+int pinctrl_init_done(struct device * dev)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/pm_runtime.h>
+
+void pm_runtime_allow(struct device * dev)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/pm_runtime.h>
+
+void pm_runtime_enable(struct device * dev)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/pm_runtime.h>
+
+void pm_runtime_forbid(struct device * dev)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/pm_runtime.h>
+
+void pm_runtime_no_callbacks(struct device * dev)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/pm_runtime.h>
+
+void pm_runtime_set_autosuspend_delay(struct device * dev,int delay)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/fs.h>
+
+int register_chrdev_region(dev_t from,unsigned count,const char * name)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+extern void register_handler_proc(unsigned int irq,struct irqaction * action);
+void register_handler_proc(unsigned int irq,struct irqaction * action)
+{
+	lx_emul_trace(__func__);
+}
+
+
+extern void register_irq_proc(unsigned int irq,struct irq_desc * desc);
+void register_irq_proc(unsigned int irq,struct irq_desc * desc)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/regulator/consumer.h>
+
+int regulator_enable(struct regulator * regulator)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/regulator/consumer.h>
+
+struct regulator * regulator_get_optional(struct device * dev,const char * id)
+{
+	lx_emul_trace(__func__);
+	return NULL;
+}
+
+
+#include <linux/reset.h>
+
+int reset_control_assert(struct reset_control * rstc)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/reset.h>
+
+int reset_control_deassert(struct reset_control * rstc)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/sysfs.h>
+
+int sysfs_add_file_to_group(struct kobject * kobj,const struct attribute * attr,const char * group)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/sysfs.h>
+
+int sysfs_create_group(struct kobject * kobj,const struct attribute_group * grp)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/sysfs.h>
+
+int sysfs_merge_group(struct kobject * kobj,const struct attribute_group * grp)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/sysfs.h>
+
+void sysfs_remove_bin_file(struct kobject * kobj,const struct bin_attribute * attr)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/sysfs.h>
+
+void sysfs_remove_dir(struct kobject * kobj)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/sysfs.h>
+
+void sysfs_remove_file_from_group(struct kobject * kobj,const struct attribute * attr,const char * group)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/sysfs.h>
+
+void sysfs_remove_file_ns(struct kobject * kobj,const struct attribute * attr,const void * ns)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/sysfs.h>
+
+void sysfs_remove_groups(struct kobject * kobj,const struct attribute_group ** groups)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/sysfs.h>
+
+void sysfs_remove_link(struct kobject * kobj,const char * name)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/sysfs.h>
+
+void sysfs_unmerge_group(struct kobject * kobj,const struct attribute_group * grp)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/fs.h>
+
+struct timespec64 current_time(struct inode * inode)
+{
+	struct timespec64 ret = { 0 };
+	lx_emul_trace(__func__);
+	return ret;
+}
+
+
+#include <linux/pid.h>
+
+void put_pid(struct pid * pid)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/cred.h>
+
+void __put_cred(struct cred * cred)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/thread_info.h>
+
+void __check_object_size(const void * ptr,unsigned long n,bool to_user)
+{
+	lx_emul_trace(__func__);
+}
+
+
+
+#include <linux/skbuff.h>
+
+void skb_init()
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/cdev.h>
+
+void cdev_init(struct cdev * cdev, const struct file_operations * fops)
+{
+	lx_emul_trace(__func__);
+}
+
+
+void synchronize_srcu(struct srcu_struct * ssp)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <asm-generic/sections.h>
+
+char __start_rodata[] = { };
+char __end_rodata[]   = { };
+
+
+#include <linux/of_reserved_mem.h>
+#include <linux/mod_devicetable.h>
+
+const struct of_device_id __reservedmem_of_table[] = {};
+
+
+#include <linux/kernel_stat.h>
+
+void account_process_tick(struct task_struct * p,int user_tick)
+{
+    lx_emul_trace(__func__);
+}
+
+
+#include <asm/memory.h>
+
+u64 vabits_actual;
+
+
+#include <linux/tracepoint-defs.h>
+
+const struct trace_print_flags vmaflag_names[]  = { {0,NULL}};
+const struct trace_print_flags pageflag_names[]  = { {0,NULL}};
+const struct trace_print_flags gfpflag_names[]  = { {0,NULL}};
+
+
+#include <linux/pci.h>
+
+unsigned long pci_address_to_pio(phys_addr_t addr)
+{
+    lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/cpuhotplug.h>
+
+int __cpuhp_setup_state(enum cpuhp_state state,const char * name,bool invoke,int (* startup)(unsigned int cpu),int (* teardown)(unsigned int cpu),bool multi_instance)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+
+#include <linux/posix-timers.h>
+
+void __init posix_cputimers_init_work(void)
+{
+    lx_emul_trace(__func__);
+}
+
+
+#include <linux/timekeeper_internal.h>
+
+void update_vsyscall(struct timekeeper * tk)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <net/net_namespace.h>
+
+void __init net_ns_init(void)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/sched/signal.h>
+
+void ignore_signals(struct task_struct * t)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/sysfs.h>
+
+int sysfs_create_bin_file(struct kobject * kobj,const struct bin_attribute * attr)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/sysfs.h>
+
+int sysfs_create_dir_ns(struct kobject * kobj,const void * ns)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/sysfs.h>
+
+int sysfs_create_file_ns(struct kobject * kobj,const struct attribute * attr,const void * ns)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/sysfs.h>
+
+int sysfs_create_groups(struct kobject * kobj,const struct attribute_group ** groups)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/sysfs.h>
+
+int sysfs_create_link(struct kobject * kobj,struct kobject * target,const char * name)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/sysfs.h>
+
+void sysfs_delete_link(struct kobject * kobj,struct kobject * targ,const char * name)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/sysfs.h>
+
+int sysfs_emit(char * buf,const char * fmt,...)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/sysfs.h>
+
+int sysfs_emit_at(char * buf,int at,const char * fmt,...)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/sysfs.h>
+
+void sysfs_notify(struct kobject * kobj,const char * dir,const char * attr)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/sysfs.h>
+
+void sysfs_remove_group(struct kobject * kobj,const struct attribute_group * grp)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/kernfs.h>
+
+struct kernfs_node * kernfs_find_and_get_ns(struct kernfs_node * parent,const char * name,const void * ns)
+{
+	lx_emul_trace(__func__);
+	return NULL;
+}
+
+
+#include <linux/kernfs.h>
+
+void kernfs_get(struct kernfs_node * kn)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/kobject.h>
+
+int kobject_uevent(struct kobject * kobj,enum kobject_action action)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/kobject.h>
+
+int kobject_uevent_env(struct kobject * kobj,enum kobject_action action,char * envp_ext[])
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/proc_fs.h>
+
+struct proc_dir_entry { int dummy; };
+struct proc_dir_entry * proc_create_seq_private(const char * name,umode_t mode,struct proc_dir_entry * parent,const struct seq_operations * ops,unsigned int state_size,void * data)
+{
+	static struct proc_dir_entry ret;
+	lx_emul_trace(__func__);
+	return &ret;
+}
+
+
+#include <linux/proc_fs.h>
+
+struct proc_dir_entry * proc_symlink(const char * name,struct proc_dir_entry * parent,const char * dest)
+{
+	lx_emul_trace(__func__);
+	return NULL;
+}
+
+
+#include <linux/pm_runtime.h>
+
+void pm_runtime_release_supplier(struct device_link * link)
+{
+	lx_emul_trace(__func__);
+}
+
+
+extern void pm_runtime_remove(struct device * dev);
+void pm_runtime_remove(struct device * dev)
+{
+	lx_emul_trace(__func__);
+}
+
+
+extern void software_node_notify(struct device * dev);
+void software_node_notify(struct device * dev)
+{
+	lx_emul_trace(__func__);
+}
+
+
+extern void software_node_notify_remove(struct device * dev);
+void software_node_notify_remove(struct device * dev)
+{
+	lx_emul_trace(__func__);
+}
+
+
+extern int dpm_sysfs_add(struct device * dev);
+int dpm_sysfs_add(struct device * dev)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/logic_pio.h>
+
+struct logic_pio_hwaddr * find_io_range_by_fwnode(struct fwnode_handle * fwnode)
+{
+	lx_emul_trace(__func__);
+	return NULL;
+}
+
+
+#include <linux/sysctl.h>
+
+struct ctl_table_header * register_sysctl(const char * path,struct ctl_table * table)
+{
+	lx_emul_trace(__func__);
+	return NULL;
+}
+
+
+#include <linux/syscore_ops.h>
+
+void register_syscore_ops(struct syscore_ops * ops)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/kernel.h>
+
+bool parse_option_str(const char * str,const char * option)
+{
+	lx_emul_trace(__func__);
+	return false;
+}
+
+
+#include <linux/sysctl.h>
+
+void __init __register_sysctl_init(const char * path,struct ctl_table * table,const char * table_name)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/rcupdate.h>
+
+void rcu_sched_clock_irq(int user)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/pm_domain.h>
+
+int dev_pm_domain_attach(struct device * dev,bool power_on)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/pm_domain.h>
+
+void dev_pm_domain_detach(struct device * dev,bool power_off)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/random.h>
+
+void add_device_randomness(const void * buf,size_t len)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/random.h>
+
+void add_interrupt_randomness(int irq)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/context_tracking_irq.h>
+
+noinstr void ct_irq_enter(void)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/context_tracking_irq.h>
+
+void ct_irq_enter_irqson(void)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/context_tracking_irq.h>
+
+noinstr void ct_irq_exit(void)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/context_tracking_irq.h>
+
+void ct_irq_exit_irqson(void)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <asm-generic/softirq_stack.h>
+
+void do_softirq_own_stack(void)
+{
+	__do_softirq();
+}
