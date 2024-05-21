26acbfb5f9643cb56534ec10d20aba3e759a868c
Josef Söntgen <josef.soentgen@genode-labs.com>
2024-05-21 18:16:26 +0200

    SQUASH FEC nic/fec: use linux-imx 6.1.20 (imx8 + imx7)

---
+++ src/drivers/nic/fec/spec/arm_v7/dummies.c
@@ -0,0 +1,80 @@
+/*
+ * \brief  Dummy definitions of Linux Kernel functions - handled manually
+ * \author Stefan Kalkowski
+ * \date   2022-10-17
+ */
+
+/*
+ * Copyright (C) 2022 Genode Labs GmbH
+ *
+ * This file is distributed under the terms of the GNU General Public License
+ * version 2.
+ */
+
+#include <lx_emul.h>
+
+
+extern void __bad_cmpxchg(volatile void *ptr, int size);
+void __bad_cmpxchg(volatile void *ptr, int size)
+{
+	lx_emul_trace_and_stop(__func__);
+}
+
+
+extern void __bad_xchg(volatile void * ptr, int size);
+void __bad_xchg(volatile void * ptr, int size)
+{
+	lx_emul_trace_and_stop(__func__);
+}
+
+
+asmlinkage void __div0(void);
+asmlinkage void __div0(void)
+{
+	lx_emul_trace_and_stop(__func__);
+}
+
+
+#include <asm/uaccess.h>
+
+unsigned long arm_copy_from_user(void *to, const void *from, unsigned long n)
+{
+	lx_emul_trace_and_stop(__func__);
+}
+
+
+#include <asm/uaccess.h>
+
+unsigned long arm_copy_to_user(void *to, const void *from, unsigned long n)
+{
+	lx_emul_trace_and_stop(__func__);
+}
+
+
+#include <linux/rcupdate.h>
+
+noinstr int notrace debug_lockdep_rcu_enabled(void)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/rcutree.h>
+
+int rcu_scheduler_active __read_mostly;
+
+
+extern void arm_heavy_mb(void);
+void arm_heavy_mb(void)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <asm-generic/bug.h>
+
+void warn_slowpath_fmt(const char * file,int line,unsigned taint,const char * fmt,...)
+{
+	lx_emul_trace(__func__);
+}
