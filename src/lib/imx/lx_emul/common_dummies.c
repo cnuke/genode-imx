3f4e14254e52a27fdd50ebb3c75b80a88ff9a324
Josef Söntgen <josef.soentgen@genode-labs.com>
2025-08-14 11:02:58 +0200

    imx: adapt dummies to 6.12 changes
    Issue genodelabs/genode#5623.

--- src/lib/imx/lx_emul/common_dummies.c
+++ src/lib/imx/lx_emul/common_dummies.c
@@ -103,3 +103,14 @@ bool is_swiotlb_allocated(void)
 	return false;
 }
 #endif
+
+
+#include <linux/initrd.h>
+
+int initrd_below_start_ok;
+
+unsigned long initrd_start, initrd_end;
+
+unsigned long phys_initrd_size;
+
+phys_addr_t phys_initrd_start;
