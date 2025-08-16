ccf56d83343670b6813ae29aa0bfc03949a2eeab
Josef Söntgen <josef.soentgen@genode-labs.com>
2025-08-16 11:09:13 +0200

    fixup 'imx: adapt dummies to 6.12 change' (fix 64bit)
    Issue genodelabs/genode#5623.

--- src/lib/imx/lx_emul/common_dummies.c
+++ src/lib/imx/lx_emul/common_dummies.c
@@ -105,6 +105,7 @@ bool is_swiotlb_allocated(void)
 #endif
 
 
+#if !defined(CONFIG_64BIT)
 #include <linux/initrd.h>
 
 int initrd_below_start_ok;
@@ -114,3 +115,4 @@ unsigned long initrd_start, initrd_end;
 unsigned long phys_initrd_size;
 
 phys_addr_t phys_initrd_start;
+#endif
